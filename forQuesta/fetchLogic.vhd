bbfc7d0cae7a14f3a9c3f665b31d8a4546552cfa
spenceropitziastate <123498250+spenceropitziastate@users.noreply.github.com>
2024-10-26 14:58:08 -0500

    Add files via upload

--- forQuesta/fetchLogic.vhd
+++ forQuesta/fetchLogic.vhd
@@ -17,6 +17,8 @@ port (
 	i_JumpInstrImm	:	in std_logic_vector(25 downto 0);	--will be shifted left, then take top 4 bits from PC
 	i_BranchInstrImm:	in std_logic_vector(N-1 downto 0);	--will be shifted left then added to PC, already sign extended
 	i_RSInput	:	in std_logic_vector(N-1 downto 0);
+	i_Clk		:	in std_logic;
+	i_Rst		:	in std_logic;
 	o_PC4		:	out std_logic_vector(N-1 downto 0);
 	o_PC		:	out std_logic_vector(N-1 downto 0)	--output program counter
 );end fetchLogic;
@@ -60,6 +62,16 @@ component andg2 is
        	o_F          : out std_logic);
 end component;
 
+component Reg32 is	--will hold PC value
+  	port(
+	i_D          : in std_logic_vector(31 downto 0);
+       	i_RST        : in std_logic;
+       	i_WE         : in std_logic;
+       	i_CLK        : in std_logic;
+       	o_Q          : out std_logic_vector(31 downto 0));
+end component;
+
+
 signal s_BranchAndZero	:	std_logic;
 signal s_UpdatedPC	:	std_logic_vector(31 downto 0); --input PC + 4
 signal s_JRMux0		:	std_logic_vector(31 downto 0);
@@ -68,6 +80,7 @@ signal s_JumpMux0	:	std_logic_vector(31 downto 0);
 signal s_JumpMux1	:	std_logic_vector(31 downto 0);
 signal s_BranchAddertemp:	std_logic_vector(31 downto 0);
 signal s_BranchMux1	:	std_logic_vector(31 downto 0);
+signal s_oldPC		:	std_logic_vector(31 downto 0);
 
 begin
 
@@ -87,6 +100,16 @@ process (i_BranchInstrImm) is --sll branch immediate 2 spots
 
 --  Diagram of fetch logic is uploaded to github
 
+PCDFFG : Reg32
+	port map(
+		i_D => i_PC,
+       		i_RST => i_Rst,
+       		i_WE => '1',
+       		i_CLK => i_Clk,
+       		o_Q => s_oldPC);
+	
+
+
 BranchAndZero:andg2
 	port map(
 		i_A     => i_Branch,
@@ -96,7 +119,7 @@ BranchAndZero:andg2
 
 AddPCto4:add_sub_n
 	port map(
-		i_A     => i_PC,
+		i_A     => s_oldPC,
 		i_B     => X"00000004", --add 4 to PC
 		i_Add_Sub => '0', --will only add
 		o_O      => s_UpdatedPC);
